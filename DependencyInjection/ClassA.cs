859bf2dd2b3b713a2262fde2b5fbdfe2485c24d7
AT <AT@DESKTOP-AKM319L>
2024-09-10 13:14:21 +0500

    Add project files.

---
+++ DependencyInjection/ClassA.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    internal class ClassA
+    {
+        private IClassA iclassA;
+        public ClassA(IClassA log)
+        {
+            this.iclassA = log;
+        }
+
+        public void Run()
+        {
+            iclassA.Method1("Message to be Printed");
+        }
+    }
+}
---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,5 @@
+﻿using DependencyInjection;
+
+var log1 = new ClassB();
+var var1 = new ClassA(log1);
+var1.Run();
